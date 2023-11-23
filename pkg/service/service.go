63ff5dda079fa1036135d5d6625891e5d31d1e16
almaz <almazsydykov768@gmail.com>
2023-11-23 15:51:54 +0600

    Changed service.go to json retrieving

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -4,28 +4,27 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/gorilla/mux"
-	"html/template"
 	"net/http"
 	"video/internal/data"
 	"video/internal/helpers"
 )
 
-var tmpl *template.Template
-
 type Application struct {
 	*sql.DB
+}
+
+type Answer struct {
+	message string
+	status  string
 }
 
 func (a Application) StreamPageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	device := vars["id"]
-	tmpl, err := template.New("").ParseFiles("cmd/api/static/templates/stream.html", "cmd/api/static/templates/base.html")
-	if err != nil {
-		fmt.Println("Problem")
-	}
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	link, _ := data.LinkModel.GetByStatusAndDevice(data.LinkModel{DB: a.DB}, true, device)
-	err = tmpl.ExecuteTemplate(w, "base", link)
+	err := helpers.WriteJSON(w, 303, link, nil)
 	if err != nil {
 		return
 	}
@@ -43,30 +42,40 @@ func (a Application) InsertHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	http.Redirect(w, r, "/", 303)
-}
-
-func (a Application) InsertPageHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.New("").ParseFiles("cmd/api/static/templates/insert.html", "cmd/api/static/templates/base.html")
-	if err != nil {
-		fmt.Println("error at insert html loader	")
+	answer := Answer{
+		message: links.Name + "was updated",
+		status:  "Ok",
 	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	err = tmpl.ExecuteTemplate(w, "base", nil)
+	err = helpers.WriteJSON(w, 303, answer, nil)
 	if err != nil {
 		return
 	}
+	http.Redirect(w, r, "/", 303)
 }
+
+// AdminPageHandler
+//
+//	func (a Application) InsertPageHandler(w http.ResponseWriter, r *http.Request) {
+//		tmpl, err := template.New("").ParseFiles("cmd/api/static/templates/insert.html", "cmd/api/static/templates/base.html")
+//		if err != nil {
+//			fmt.Println("error at insert html loader	")
+//		}
+//		w.Header().Set("Access-Control-Allow-Origin", "*")
+//		err = tmpl.ExecuteTemplate(w, "base", nil)
+//		if err != nil {
+//			return
+//		}
+//	}
 func (a Application) AdminPageHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.New("").ParseFiles("cmd/api/static/templates/adminpage.html", "cmd/api/static/templates/base.html")
 	links, err := data.LinkModel.GetAllLinks(data.LinkModel{DB: a.DB})
 	if err != nil {
 		fmt.Println(err)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	err = tmpl.ExecuteTemplate(w, "base", links)
+	err = helpers.WriteJSON(w, 303, links, nil)
 	if err != nil {
 		return
 	}
